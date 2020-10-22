4ce5d05c08284f98f62595e5c8a2da9aa8527ed2
陈志龙 <736737261@qq.com>
2020-10-22 18:09:34 +0800

    2020-10-22 First Commit

---
+++ module_login/src/main/java/com/czl/module_login/ui/login/LoggedInUserView.kt
@@ -0,0 +1,9 @@
+package com.czl.module_login.ui.login
+
+/**
+ * User details post authentication that is exposed to the UI
+ */
+data class LoggedInUserView(
+    val displayName: String
+    //... other data fields that may be accessible to the UI
+)
